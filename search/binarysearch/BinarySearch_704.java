5f55b75fd4a13a26d52f67368c94fd5a6ca3ebc5
czhang27 <czhang27@trip.com>
2021-08-17 21:33:13 +0800

    111

--- search/binarysearch/BinarySearch_704.java
+++ search/binarysearch/BinarySearch_704.java
@@ -15,6 +15,7 @@ public class BinarySearch_704 {
         public int search(int[] nums, int target) {
             int left = 0;
             int right = nums.length - 1;
+
             while (left <= right) {
                 int mid = (right - left) / 2 + left;
                 if (nums[mid] > target) {
