eda08d03d3d75a474ce0376cd49d3d148edaa24c
czhang27 <czhang27@trip.com>
2021-08-17 21:44:04 +0800

    二分

---
+++ search/binarysearch/FirstBadVersion_278.java
@@ -0,0 +1,50 @@
+package leetcode.search.binarysearch;
+
+/**
+ * The isBadVersion API is defined in the parent class VersionControl.
+ *       boolean isBadVersion(int version);
+ */
+public class FirstBadVersion_278 {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        System.out.println(new Solution().firstBadVersion(5));
+
+
+    }
+
+    /**
+     * 你的产品的最新版本没有通过质量检测。
+     * 由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+     */
+
+
+    public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int left = 1;
+            int right = n;
+            while (left < right) {
+                int mid = (right - left) / 2 + left;
+                if (isBadVersion(mid)) {
+                    right = mid;
+                }
+                else {
+                    // left mid bad...bad right
+                    left = mid + 1;
+                }
+            }
+            // left = right
+            return left;
+        }
+    }
+
+
+
+
+
+
+
+
+}
