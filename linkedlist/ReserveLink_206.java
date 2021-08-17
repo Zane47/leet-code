b52f5adcd9b7bc094d322133605db5c8e82c3f50
czhang27 <czhang27@trip.com>
2021-08-17 20:59:02 +0800

    111

--- linkedlist/ReserveLink_206.java
+++ linkedlist/ReserveLink_206.java
@@ -1,32 +1,61 @@
 package leetcode.linkedlist;
 
-public class ReserveLink {
+import java.util.Objects;
+
+public class ReserveLink_206 {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println(new Solution().reverseList(head).val);
+    }
 
 
-    public class Solution {
-        public ListNode ReverseList(ListNode head) {
 
+    static class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode currNode = head;
+            ListNode nextNode = null;
+            ListNode preNode = null;
 
+            while (currNode != null) {
+                nextNode = currNode.next;
+                currNode.next = preNode;
+                preNode = currNode;
+                currNode = nextNode;
+            }
 
-            return null;
+            return preNode;
         }
     }
 
 
-    public class ListNode {
-        int val;
-        ListNode next = null;
 
-        ListNode(int val) {
-            this.val = val;
-        }
-    }
 
 
 }
 
 
+class ListNode {
 
+    int val;
+
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 /*
 
---
+++ search/binarysearch/BinarySearch_704.java
@@ -0,0 +1,35 @@
+package leetcode.search.binarysearch;
+
+public class BinarySearch_704 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {-1, 0, 3, 5, 9, 12};
+
+        int target = 9;
+        // int target = 2;
+
+        System.out.println(new Solution().search(nums, target));
+    }
+
+    static class Solution {
+        public int search(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = (right - left) / 2 + left;
+                if (nums[mid] > target) {
+                    right = mid - 1;
+                }
+                else if (nums[mid] < target) {
+                    left = mid + 1;
+                }
+                else if (nums[mid] == target) {
+                    return mid;
+                }
+            }
+            return -1;
+        }
+    }
+
+
+}
