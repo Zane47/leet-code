90bae0eed161918907247b2fa1a588e5c6ca7455
zhangc47 <1228073191@qq.com>
2021-09-22 15:51:55 +0800

    Update Minesweeper_529.java

--- dfsandbfs/Minesweeper_529.java
+++ dfsandbfs/Minesweeper_529.java
@@ -1,5 +1,9 @@
 package leetcode.dfsandbfs;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  *
@@ -77,10 +81,76 @@ public class Minesweeper_529 {
         };
         int[] click = new int[]{3, 0};
 
+        System.out.println(Arrays.deepToString(new Solution().updateBoard(board, click)));
     }
 
+
     /**
-     * 模拟
+     * 模拟+bfs
+     */
+    static class Solution {
+        int[] dx = new int[]{-1, 1, 0, 0, -1, -1, 1, 1};
+        int[] dy = new int[]{0, 0, -1, 1, -1, 1, -1, 1};
+
+        public char[][] updateBoard(char[][] board, int[] click) {
+            int x = click[0];
+            int y = click[1];
+
+            // 规则1：如果一个地雷（'M'）被挖出，游戏就结束了- 把它改为 'X'。
+            if (board[x][y] == 'M') {
+                board[x][y] = 'X';
+                return board;
+            }
+
+            // 点击了E
+            // bfs
+            boolean[][] isVisited = new boolean[board.length][board[0].length];
+            Queue<int[]> queue = new LinkedList<>();
+            queue.offer(new int[]{x, y});
+            isVisited[x][y] = true;
+            while (!queue.isEmpty()) {
+                int[] point = queue.poll();
+                int i = point[0];
+                int j = point[1];
+                int count = 0;
+
+                // 判断周围是否有雷
+                for (int v1 = 0; v1 < 8; v1++) {
+                    int newX = i + dx[v1];
+                    int newY = j + dy[v1];
+                    if (newX >= 0 && newX < board.length && newY >= 0 && newY < board[0].length
+                            && board[newX][newY] == 'M') {
+                        count++;
+                    }
+                }
+                // 若空地 (i, j) 周围有雷，则将该位置修改为雷数；
+                // 否则将该位置更新为 ‘B’，并将其 8 邻域中的空地入队，继续进行 bfs 搜索。
+                if (count > 0) {
+                    // (i, j) 周围有雷
+                    board[i][j] = (char) ('0' + count);
+                } else {
+                    // (i, j) 周围没雷
+                    board[i][j] = 'B';
+                    // 将剩下的8个相邻的空地加入queue
+                    for (int v1 = 0; v1 < 8; v1++) {
+                        int newX = i + dx[v1];
+                        int newY = j + dy[v1];
+                        if (newX >= 0 && newX < board.length && newY >= 0 && newY < board[0].length
+                                && board[newX][newY] == 'E' && !isVisited[newX][newY]) {
+                            // 注意不要重复入queue
+                            isVisited[newX][newY] = true;
+                            queue.offer(new int[]{newX, newY});
+                        }
+                    }
+                }
+            }
+            return board;
+        }
+    }
+
+
+    /**
+     * 模拟+dfs
      *
      *
      * 总共分两种情况:
@@ -107,7 +177,7 @@ public class Minesweeper_529 {
      * 来源：力扣（LeetCode）
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      */
-    static class Solution {
+    static class Solutiondfs {
         int[] dx = new int[]{-1, 1, 0, 0, -1, -1, 1, 1};
         int[] dy = new int[]{0, 0, -1, 1, -1, 1, -1, 1};
 
@@ -119,12 +189,10 @@ public class Minesweeper_529 {
             if (board[x][y] == 'M') {
                 board[x][y] = 'X';
             } else {
-                // 点击了E
+                // 点击了E ->
                 dfs(board, x, y);
             }
-
             return board;
-
         }
 
         private void dfs(char[][] board, int i, int j) {
@@ -133,13 +201,10 @@ public class Minesweeper_529 {
             // 递归终止条件：
             // 判断空地 (i, j) 周围是否有雷，若有，则将该位置修改为雷数，终止该路径的搜索。
             for (int v1 = 0; v1 < 8; v1++) {
-                int x = i + dx[i];
-                int y = j + dy[i];
+                int x = i + dx[v1];
+                int y = j + dy[v1];
                 // 越界则到下一个位置
-                if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) {
-                    continue;
-                }
-                if (board[x][y] == 'M') {
+                if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] == 'M') {
                     count++;
                 }
             }
@@ -148,10 +213,14 @@ public class Minesweeper_529 {
                 return;
             }
 
-            // 如果旁边没有雷，那么就要去其他的空地进行搜索
+            // 如果旁边没有雷，则将该位置修改为 ‘B，去其他的相邻空地进行搜索
             board[i][j] = 'B';
-            for () {
-
+            for (int v1 = 0; v1 < 8; v1++) {
+                int x = i + dx[v1];
+                int y = j + dy[v1];
+                if (x >=0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] == 'E') {
+                    dfs(board, x, y);
+                }
             }
 
 
