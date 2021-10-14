f56a24978bb8cdf31d137b3d1c96dd578171b516
zhangc47 <1228073191@qq.com>
2021-10-15 00:06:30 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # leetcode
+
+## 图形化演示
+https://www.cs.usfca.edu/~galles/visualization/Algorithms.html
--- searchfunc/BinarySearch.java
+++ searchfunc/BinarySearch.java
@@ -8,31 +8,28 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         ArrayList<Integer> list = createList();
-
         Collections.sort(list);
-
+        // 有序序列才可以用二分查找
         binarySearch(list, 20);
-
-
     }
 
-    private static void binarySearch(ArrayList<Integer> list, Integer index) {
+    private static void binarySearch(ArrayList<Integer> list, Integer target) {
         int left = 0;
         int right = list.size() - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (index < list.get(mid)) {
+            if (target < list.get(mid)) {
                 right = mid - 1;
             }
-            else if (index > list.get(mid)) {
+            else if (target > list.get(mid)) {
                 left = mid + 1;
             }
             else {
-                System.out.println("find it!" + index + "at position: " + mid);
+                System.out.println("find it!" + target + "at position: " + mid);
                 return;
             }
         }
-        System.out.println("not find! " + index);
+        System.out.println("not find! " + target);
 
 
     }
