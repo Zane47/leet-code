2750e546912a045b831d17a4553ebf9c7dc477b5
zhangc <1228073191@qq.com>
2021-11-10 16:43:12 +0800

    1

--- dp/subsequence/MaximumSubarray_53.java
+++ dp/subsequence/MaximumSubarray_53.java
@@ -1,9 +1,10 @@
 package leetcode.dp.subsequence;
 
-import java.util.Arrays;
-
 /**
  * 53.最大子序和
+ *
+ * -> pat 1007 注意其中的变化
+ *
  * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和
  * <p>
  * <p>
