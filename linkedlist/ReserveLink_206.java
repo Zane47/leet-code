814393a71211dfd5873121234d3c09db5cdbadb7
zhangc <1228073191@qq.com>
2021-11-11 16:35:23 +0800

    1

--- linkedlist/ReserveLink_206.java
+++ linkedlist/ReserveLink_206.java
@@ -34,28 +34,28 @@ public class ReserveLink_206 {
 
 
 
+    static class ListNode {
 
+        int val;
+
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 
 }
 
 
-class ListNode {
 
-    int val;
-
-    ListNode next;
-
-    ListNode() {}
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
 
 /*
 
---
+++ linkedlist/ReverseNodesInKGroup_25.java
@@ -0,0 +1,62 @@
+package leetcode.linkedlist;
+
+/**
+ * 给你一个链表，每k个节点一组进行翻转，请你返回翻转后的链表。
+ *
+ * k是一个正整数，它的值小于或等于链表的长度。
+ *
+ * 如果节点总数不是k的整数倍，那么请将最后剩余的节点保持原有顺序。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ReverseNodesInKGroup_25 {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println(new Solution().reverseKGroup(head, 2));
+    }
+
+    /**
+     * https://github.com/labuladong/fucking-algorithm/blob/master/%E9%AB%98%E9%A2%91%E9%9D%A2%E8%AF%95%E7%B3%BB%E5%88%97/k%E4%B8%AA%E4%B8%80%E7%BB%84%E5%8F%8D%E8%BD%AC%E9%93%BE%E8%A1%A8.md
+     */
+    static class Solution {
+        public ListNode reverseKGroup(ListNode head, int k) {
+
+
+            reverse(head);
+
+            return null;
+        }
+
+        private void reverse(ListNode head) {
+            ListNode pre = null, cur = head, next = head;
+
+
+
+
+
+        }
+    }
+
+
+
+    /**
+     * Definition for singly-linked list.
+     */
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
+
+
+
