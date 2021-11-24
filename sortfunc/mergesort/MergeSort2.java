f20fd372c68dfc95bb1429281af2c03449c77903
zhangc47 <1228073191@qq.com>
2021-11-24 20:19:41 +0800

    Create MergeSort2.java

---
+++ sortfunc/mergesort/MergeSort2.java
@@ -0,0 +1,13 @@
+package leetcode.sortfunc.mergesort;
+
+/**
+ * https://blog.csdn.net/jianyuerensheng/article/details/51262984
+ * https://blog.csdn.net/CmdSmith/article/details/71405740
+ */
+public class MergeSort2 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
