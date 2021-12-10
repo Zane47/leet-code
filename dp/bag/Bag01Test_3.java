c7a4958dea37ce7189c4d36ea37381e410e6bf27
Zane47 <1228073191@qq.com>
2021-12-10 13:59:02 +0800

    1

--- dp/bag/Bag01Test_3.java
+++ dp/bag/Bag01Test_3.java
@@ -1,5 +1,7 @@
 package leetcode.dp.bag;
 
+import java.util.Arrays;
+
 /**
  * 一维dp数组
  * 空间复杂度 O(V)
@@ -12,8 +14,11 @@ public class Bag01Test_3 {
         testWeightBagProblem3(weight.length, weight, value, V);
     }
 
+    /**
+     * dp[j]表示背包容量为j时，能获得的最大价值
+     */
     private static void testWeightBagProblem3(int n, int[] weight, int[] value, int V) {
-        //定义dp数组：dp[j]表示背包容量为j时，能获得的最大价值
+        // 定义dp数组：dp[j]表示背包容量为j时，能获得的最大价值
         int[] dp = new int[V + 1];
 
         //遍历顺序：先遍历物品，再遍历背包容量
@@ -24,10 +29,9 @@ public class Bag01Test_3 {
                 // 2.取dp[j - weight[i]] + value[i]，即放物品i，
                 dp[v] = Math.max(dp[v], dp[v - weight[i]] + value[i]);
             }
-        }
-        //打印dp数组
-        for (int v = 0; v <= V; v++) {
-            System.out.print(dp[v] + " ");
+
+            // 打印dp数组
+            System.out.println(i + ":" + Arrays.toString(dp));
         }
 
 
