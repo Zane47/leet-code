ff22e51ed46b9780d9a5d1cd60f0e25e9492907b
Zane47 <1228073191@qq.com>
2022-01-06 14:11:47 +0800

    杨辉三角模拟

--- dp/PascalsTriangle_118.java
+++ dp/PascalsTriangle_118.java
@@ -2,8 +2,13 @@ package leetcode.dp;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * 杨辉三角
+ */
 public class PascalsTriangle_118 {
     public static void main(String[] args) {
         int numRows = 5;
@@ -12,15 +17,45 @@ public class PascalsTriangle_118 {
     }
 
 
+    /**
+     * 模拟解法
+     */
     static class Solution {
         public List<List<Integer>> generate(int numRows) {
-            List<List<Integer>> dp = new ArrayList<>();
+            List<List<Integer>> result = new ArrayList<>();
 
-            // 初始化,
-            // 第一位和最后一位为1
+            // 初始化第一行
+            List<Integer> init = new ArrayList<>();
+            init.add(1);
+            result.add(init);
+            if (numRows == 1) {
+                return result;
+            }
+            init = new ArrayList<>();
+            init.add(1);
+            init.add(1);
+            result.add(init);
+            if (numRows == 2) {
+                return result;
+            }
+
+            for (int i = 3; i <= numRows; i++) {
+                int[] nums = new int[i];
+                // 第一位和最后位是1
+                nums[0] = 1;
+                nums[i - 1] = 1;
+
+                // 第i行的[1, i-1]位
+                for (int j = 1; j <= i - 2; j++) {
+                    nums[j] = result.get(i - 2).get(j - 1) + result.get(i - 2).get(j);
+                }
+
+                List<Integer> collect = Arrays.stream(nums).boxed().collect(Collectors.toList());
+                result.add(collect);
+            }
 
 
-            return dp;
+            return result;
         }
     }
 }
