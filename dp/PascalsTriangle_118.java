f7249fb236cea3f86c96ae64283c712d9927fce0
Zane47 <1228073191@qq.com>
2022-01-06 11:22:18 +0800

    1

---
+++ dp/PascalsTriangle_118.java
@@ -0,0 +1,26 @@
+package leetcode.dp;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle_118 {
+    public static void main(String[] args) {
+        int numRows = 5;
+
+        System.out.println(new Solution().generate(numRows));
+    }
+
+
+    static class Solution {
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> dp = new ArrayList<>();
+
+            // 初始化,
+            // 第一位和最后一位为1
+
+
+            return dp;
+        }
+    }
+}
