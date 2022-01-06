90ad6e354fb1925d64adb22fd0bbbbce0a74e4c2
Zane47 <1228073191@qq.com>
2022-01-06 16:03:49 +0800

    buyandsellstock3

--- dp/sellstock/BuyAndSellStock3_123.java
+++ dp/sellstock/BuyAndSellStock3_123.java
@@ -11,13 +11,13 @@ public class BuyAndSellStock3_123 {
     }
 
     /**
-     * dp[i][k][0]
-     * dp[i][k][1]
+     * dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+     * dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
      *
      * # 状态转移:
      * k = 2;
      *
-     * dp[i][2][0] = max(dp[i-1][2][0], dp[i-1][2][1] + prices[i]);
+     * dp[i][2][0] = max(dp[i-1][2][0], dp[i-1][2][1] + prices[i])
      *
      * dp[i][2][1] = max(dp[i-1][2][1], dp[i-1][1][0] - prices[i])
      *
@@ -26,7 +26,7 @@ public class BuyAndSellStock3_123 {
      *
      * dp[0][2][1] = max(dp[-1][2][1], dp[-1][1][0] - prices[0])
      *
-     *
+     * 这里对k也要进行穷举, 遍历状态
      *
      *
      */
