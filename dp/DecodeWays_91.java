19b0a62a7ac322917339bc88793d99b3ec094579
Zane47 <1228073191@qq.com>
2022-01-13 11:21:55 +0800

    11

---
+++ dp/DecodeWays_91.java
@@ -0,0 +1,32 @@
+package leetcode.dp;
+
+/**
+ * 要 解码 已编码的消息，所有数字必须基于上述映射的方法，反向映射回字母（可能有多种方法）。例如，"11106" 可以映射为：
+ *
+ * "AAJF" ，将消息分组为 (1 1 10 6)
+ * "KJF" ，将消息分组为 (11 10 6)
+ * 注意，消息不能分组为 (1 11 06) ，
+ * 因为 "06" 不能映射为 "F" ，这是由于 "6" 和 "06" 在映射中并不等价。
+ *
+ * 给你一个只含数字的 非空 字符串 s ，请计算并返回 解码 方法的 总数 。
+ *
+ */
+public class DecodeWays_91 {
+    public static void main(String[] args) {
+        String s = "11106";
+
+        System.out.println(s);
+    }
+
+
+    static class Solution {
+        public int numDecodings(String s) {
+
+
+
+            return 0;
+        }
+    }
+
+
+}
