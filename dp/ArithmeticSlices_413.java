91689a8640d9f43430ca29b8073fd56b7afe138b
Zane47 <1228073191@qq.com>
2022-01-13 10:49:52 +0800

    等差数列413

--- dp/ArithmeticSlices_413.java
+++ dp/ArithmeticSlices_413.java
@@ -2,16 +2,74 @@ package leetcode.dp;
 
 public class ArithmeticSlices_413 {
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3, 4};
-        System.out.println(new Solution().numberOfArithmeticSlices(nums));
+        // int[] nums = new int[]{1, 2, 3, 4, 5, 7};
+
+        int[] nums = new int[]{1, 2, 3, 4, 5, 7, 8, 9, 10};
+
+        System.out.println(new Solution2().numberOfArithmeticSlices(nums));
     }
 
+
+    /**
+     * dp
+     */
+    static class Solution {
+        public int numberOfArithmeticSlices(int[] nums) {
+            int length = nums.length;
+            int dp = 0;
+            int result = 0;
+
+            for (int i = 2; i < length; i++) {
+                if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                    dp++;
+                } else {
+                    dp = 0;
+                }
+                // System.out.print(dp + " ");
+
+                result += dp;
+            }
+
+            // System.out.println();
+            return result;
+        }
+    }
+
+    static class Solution2 {
+        public int numberOfArithmeticSlices(int[] nums) {
+            int length = nums.length;
+            int result = 0;
+
+            if (length < 3) {
+                return 0;
+            }
+
+            int[] dp = new int[length];
+            dp[0] = 0;
+            dp[1] = 0;
+
+            for (int i = 2; i < length; i++) {
+                if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                    dp[i] = dp[i - 1] + 1;
+                } else {
+                    dp[i] = 0;
+                }
+
+                // System.out.print(dp[i] + " ");
+
+                result += dp[i];
+            }
+
+            // System.out.println();
+            return result;
+        }
+    }
 
 
     /**
      * 直接O(n^2)
      */
-    static class Solution {
+    static class Solution1 {
         public int numberOfArithmeticSlices(int[] nums) {
             int length = nums.length;
             if (length < 3) {
