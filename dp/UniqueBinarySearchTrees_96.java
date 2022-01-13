740769fcadfbaa5b18df78dc822f5f0e1800e499
Zane47 <1228073191@qq.com>
2022-01-13 16:42:03 +0800

    1

---
+++ dp/UniqueBinarySearchTrees_96.java
@@ -0,0 +1,16 @@
+package leetcode.dp;
+
+public class UniqueBinarySearchTrees_96 {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(new Solution().numTrees(n));
+    }
+
+    static class Solution {
+        public int numTrees(int n) {
+
+
+            return 0;
+        }
+    }
+}
