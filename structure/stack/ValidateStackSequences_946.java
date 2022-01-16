9018f6ab0457bf77a50280db69c6f83049d0a129
Zane47 <1228073191@qq.com>
2022-01-16 14:56:41 +0800

    栈pushpop序列验证

--- structure/stack/ValidateStackSequences_946.java
+++ structure/stack/ValidateStackSequences_946.java
@@ -6,6 +6,16 @@ import java.util.Stack;
 /**
  * [1,2,3,4,5]
  * [4,5,3,2,1]
+ * pop序列是否是push序列
+ *
+ * 遍历push序列, 只要和pop序列不一致就push, pop和栈顶一致就pop,
+ * 最后看是否栈空
+ *
+ * 将 pushed 队列中的每个数都 push 到栈中，同时检查这个数是不是 popped 序列中下一个要 pop 的值，如果是就把它 pop 出来。
+ *
+ * 最后，检查不是所有的该 pop 出来的值都是 pop 出来了。
+ *
+ *
  */
 public class ValidateStackSequences_946 {
     public static void main(String[] args) {
