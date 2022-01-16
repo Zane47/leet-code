1f7c31674bc960eecea697438e69297ae3d6e505
Zane47 <1228073191@qq.com>
2022-01-16 14:16:26 +0800

    计算存的钱, 等差数列

--- simulation/CalculateMoneyInLeetcodeBank_1716.java
+++ simulation/CalculateMoneyInLeetcodeBank_1716.java
@@ -2,17 +2,42 @@ package leetcode.simulation;
 
 public class CalculateMoneyInLeetcodeBank_1716 {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 18;
         System.out.println(new Solution().totalMoney(n));
     }
+
+
+    /**
+     * 等差数列求和优化
+     * <p>
+     * 首项 + 末项 乘以 项数 除以 2
+     */
+    static class Solution {
+        public int totalMoney(int n) {
+            // 计算完整周的金钱
+            // 完整的周
+            int fullWeekNum = n / 7;
+            int result1 = (28 + 28 + 7 * (fullWeekNum-1)) * fullWeekNum / 2;
+
+            // 零散周的金钱
+            // 最后一行剩余的元素个数
+            int colLeft = n % 7;
+            int result2 = (fullWeekNum + 1 + fullWeekNum + 1 + colLeft - 1) * colLeft / 2;
+
+            return result1 + result2;
+        }
+    }
+
 
     /**
      * 创建二维数组即可,
      * n/7行, 7列
+     * 每一行都比上一行的和大7
      */
-    static class Solution {
+    static class Solution1 {
         public int totalMoney(int n) {
             int col = 7;
+            // 存了几周的钱
             int row = n / 7 + 1;
 
             int[][] nums = new int[row][col];
@@ -23,13 +48,19 @@ public class CalculateMoneyInLeetcodeBank_1716 {
                 }
             }
 
+            int result = 0;
+            // 求和
             for (int i = 0; i < row; i++) {
                 for (int j = 0; j < col; j++) {
-                    
+                    if (n > 0) {
+                        result += nums[i][j];
+                        n--;
+                    }
                 }
-                
             }
-            return 0;
+
+
+            return result;
         }
     }
 }
