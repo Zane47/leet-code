0973d6857f590fb5bf00a60ac16a42cc97da1290
Zane47 <1228073191@qq.com>
2023-04-01 11:12:40 +0800

    rename

--- twopoints/IsPalindrome2_LintCode891.java
+++ twopoints/IsPalindrome2_LintCode891.java
@@ -1,4 +1,15 @@
 package leetcode.twopoints;
 
-public class IsPalindrome_LintCode891 {
+
+public class IsPalindrome2_LintCode891 {
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
+
 }
